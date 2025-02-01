d76ff3cb822ee809246eeab563613488b2aae0da
jhonatanbarjag <jhonatanbarja.g@hotmail.com>
2025-01-31 19:52:36 -0500

    Realizar portafolio

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+
+router.get("/", (req, res) => res.render("index", {title :"inicio"}));
+router.get("/about", (req, res) => res.render("sobremi", {title :"sobre mi"}));
+router.get("/habilidad", (req, res) => res.render("habilidades", {title :"habilidades"}));
+router.get("/contacto", (req, res) => res.render("Contacto", {title :"contacto"}));
+
+
+export default router;
