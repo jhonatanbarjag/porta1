4568cbc9c643a119953fefb11f772bf099ad2c4c
jhonatanbarjag <jhonatanbarja.g@hotmail.com>
2025-01-31 21:34:41 -0500

    Realizar cambio nombre

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get("/", (req, res) => res.render("index", {title :"inicio"}));
 router.get("/about", (req, res) => res.render("sobremi", {title :"sobre mi"}));
 router.get("/habilidad", (req, res) => res.render("habilidades", {title :"habilidades"}));
-router.get("/contacto", (req, res) => res.render("Contacto", {title :"contacto"}));
+router.get("/contacto", (req, res) => res.render("contacto", {title :"contacto"}));
 
 
 export default router;
